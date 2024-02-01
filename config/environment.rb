bb83fac2316b45b3b26f023441128b415fc8c110
JackQuintonS12 <145986615+JackQuintonS12@users.noreply.github.com>
2024-02-01 16:10:59 +0000

    Add logging

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,9 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+Rails.logger = Logger.new(STDOUT)
+
+Rails.logger.level = Logger::INFO
+
+Rails.logger.datetime_format = '%Y-%m-%d %H:%M:%S'
